e5a960c17403f6674c5b86536f72314052aaa576
unknown <arestre@ncsu.edu>
2022-06-08 17:21:15 -0400

    array set up

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const startButton = document.getElementById('start-btn')
 const questionContainerElement = document.getElementById
 ('question-container')
+const questionElement = document.getElementById("question")
+const answerButtonsElement = document.getElementById("answer-buttons")
+
+const shuffledQuestions, currentQuestionIndex
+
+var colombia = "https://cdn.britannica.com/68/7668-004-08492AB7/Flag-Colombia.jpg"
 
 startButton.addEventListener('click', startQuiz)
 
@@ -9,14 +15,33 @@ startButton.addEventListener('click', startQuiz)
 function startQuiz() {
     console.log('Started')
     startButton.classList.add('hide')
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
     questionContainerElement.classList.remove('hide')
+    setNextQuestion()
 
 }
 
 function setNextQuestion() {
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+
+}
+
+function showQuestion(question) {
+    questionElement.innerText = question.question
 
 }
 
 function selectAnswer() {
 
 }
+
+const question = [
+    {
+        question: 'what is this' ,
+        answers: [
+            {text: 'Colombia', correct: true},
+            {text: 'Bolivia', correct: false}
+        ]
+    }
+]
